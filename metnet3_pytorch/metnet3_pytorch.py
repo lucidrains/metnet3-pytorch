945bf40fb4544ff0946053377eb152dffdce07c8
Phil Wang <lucidrains@gmail.com>
2023-11-04 10:01:06 -0700

    bring in maxvit with register tokens (meta ai improvement to vit)

--- metnet3_pytorch/metnet3_pytorch.py
+++ metnet3_pytorch/metnet3_pytorch.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn, Tensor
 from torch.nn import Module, ModuleList
 
-from vit_pytorch.maxvit import MaxViT
+from vit_pytorch.maxvit_with_registers import MaxViT
 
 class MetNet3(Module):
     raise NotImplementedError
