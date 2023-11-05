976e02a59629518445f1c4200f27385a82093ea2
Phil Wang <lucidrains@gmail.com>
2023-11-05 10:03:47 -0800

    just copy in a resnet block

--- metnet3_pytorch/metnet3_pytorch.py
+++ metnet3_pytorch/metnet3_pytorch.py
@@ -25,6 +25,58 @@ def unpack_one(x, ps, pattern):
 def cast_tuple(val, length = 1):
     return val if isinstance(val, tuple) else ((val,) * length)
 
+# conditionable resnet block
+
+class Block(Module):
+    def __init__(self, dim, dim_out, groups = 8):
+        super().__init__()
+        self.proj = nn.Conv2d(dim, dim_out, 3, padding = 1)
+        self.norm = nn.GroupNorm(groups, dim_out)
+        self.act = nn.ReLU()
+
+    def forward(self, x, scale_shift = None):
+        x = self.proj(x)
+        x = self.norm(x)
+
+        if exists(scale_shift):
+            scale, shift = scale_shift
+            x = x * (scale + 1) + shift
+
+        x = self.act(x)
+        return x
+
+class ResnetBlock(Module):
+    def __init__(self, dim, dim_out, *, time_emb_dim = None, groups = 8):
+        super().__init__()
+        self.mlp = None
+
+        if exists(time_emb_dim):
+            self.mlp = Sequential(
+                nn.ReLU(),
+                nn.Linear(time_emb_dim, dim_out * 2)
+            )
+
+        self.block1 = Block(dim, dim_out, groups = groups)
+        self.block2 = Block(dim_out, dim_out, groups = groups)
+        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
+
+    def forward(self, x, time_emb = None):
+
+        scale_shift = None
+
+        assert not (exists(self.mlp) ^ exists(time_emb))
+
+        if exists(self.mlp) and exists(time_emb):
+            time_emb = self.mlp(time_emb)
+            time_emb = rearrange(time_emb, 'b c -> b c 1 1')
+            scale_shift = time_emb.chunk(2, dim = 1)
+
+        h = self.block1(x, scale_shift = scale_shift)
+
+        h = self.block2(h)
+
+        return h + self.res_conv(x)
+
 # multi-headed rms normalization, for query / key normalized attention
 
 class RMSNorm(Module):
